e6d09e2cc80790566088d1f979ef833a5e3dffcb
Rafael Lua <50277486+RafaelLua13@users.noreply.github.com>
2019-10-06 11:31:47 -0300

    Create 1003.py

---
+++ 1003.py
@@ -0,0 +1,15 @@
+## Código ##
+
+A = int(input())
+B = int(input())
+S = A + B
+print ("SOMA =", S)
+
+## Código comentado ##
+
+
+#   A = int(input())     # Definição das variáveis 'A' e 'B' como escolha do usuário.     
+#   B = int(input())     # O usuário deve digitar um numero inteiro (int) nos 'inputs' das variáveis, 
+                         #    -> caso contrário, o programa não funcionará.
+#   S = A + B            # Armazenar na variável 'S' a soma dos valores escolhidos de 'A' e 'B'. 
+#   print("SOMA =", S)   # Mostrar o resultado da Soma
