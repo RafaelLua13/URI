cb60392039ee6f485844e0ca5c7456d573b6fc65
Rafael Lua <50277486+RafaelLua13@users.noreply.github.com>
2019-10-06 15:33:27 -0300

    Create 1005.py

---
+++ 1005.py
@@ -0,0 +1,17 @@
+## Código ##
+
+A = float(input())
+B = float(input())
+M = (A*3.5) + (B*7.5)
+MF = (M/11)
+print ("MEDIA = %.5f" %MF)
+
+
+
+## Código comentado ##
+
+#   A = float(input())           # Definição das variáveis 'A' e 'B' como um numero float (Não necessáriamente inteiro).    
+#   B = float(input())
+#   M = (A*3.5) + (B*7.5)        # Calculo de 'M' somando as multiplicações de A * 3.5 com B * 7.5. 
+#   MF = (M/11)                  # Média Final (MF) é a divisão de 'M' por 11.
+#   print ("MEDIA = %.5f" %MF)   # Mostrar a Média Final com somente 5 casas decimais (%.5f). 
