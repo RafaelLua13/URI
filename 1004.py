38d0b1dd15e20aa8886308b15c11b358b43d81b8
Rafael Lua <50277486+RafaelLua13@users.noreply.github.com>
2019-10-06 11:32:33 -0300

    Create 1004.py

---
+++ 1004.py
@@ -0,0 +1,15 @@
+  
+## Código ##
+
+a = int(input())
+b = int(input())
+x = a * b
+print("PROD =",x)
+
+
+## Código comentado ##
+
+#   a = int(input())   # Definição das variáveis 'a' e 'b' como um valor inteiro. 
+#   b = int(input())   # O usuário deve digitar um valor inteiro para o programa funcionar.
+#   x = a * b          # Armazenar em 'x' a multiplicação das variáveis 'a' e 'b'.
+#   print("PROD =",x)  # Mostrar o resultado de 'x'.
