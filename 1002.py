bf8b1da28c1a283a7b62bf28614a3968aa9930b7
Rafael Lua <50277486+RafaelLua13@users.noreply.github.com>
2019-10-06 11:31:11 -0300

    Create 1002.py

---
+++ 1002.py
@@ -0,0 +1,12 @@
+  
+## Código ##
+
+r = float(input())
+a = (r  * r) * 3.14159
+print("A=%0.4f" %a)
+
+## Código comentado ##
+
+#   r = float(input())         #  Definição da variável 'r' como um valor float (Não necessáriamente inteiro).
+#   a = (r  * r) * 3.14159     #  Cálculo da área do circulo multiplicando r^2 (r**2) pelo valor de Pi (3.14159). 
+#   print("A=%0.4f" %a)        #  Mostrando a variavel 'a' com apenas 4 casas decimais (%0.4f).
