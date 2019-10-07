30ff6a321118b0a1a69c3bb5b5f3a36dcada20fd
RafaelLua13 <rafael.lua6@gmail.com>
2019-10-07 13:07:49 -0300

    Teste de commit

--- Python/1005.py
+++ Python/1005.py
@@ -15,3 +15,6 @@ print ("MEDIA = %.5f" %MF)
 #   M = (A*3.5) + (B*7.5)        # Calculo de 'M' somando as multiplicações de A * 3.5 com B * 7.5. 
 #   MF = (M/11)                  # Média Final (MF) é a divisão de 'M' por 11.
 #   print ("MEDIA = %.5f" %MF)   # Mostrar a Média Final com somente 5 casas decimais (%.5f). 
+
+
+# Teste
